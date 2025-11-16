52d29afd085c0d2f34967505f3adc063a965ffc4
bwanswa <deogratiousbwanswa@gmail.com>
2025-11-15 21:36:29 -0800

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBRyHQf2IWzPoOrm8UsgcdJvDIxEQR2G40",
+    authDomain: "asa1db.firebaseapp.com",
+    projectId: "asa1db",
+    storageBucket: "asa1db.firebasestorage.app",
+    messagingSenderId: "195882381688",
+    appId: "1:195882381688:web:88e69407ef003bb8c7188d"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    console.log(user);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const githubProvider = new GithubAuthProvider();
+const signInWithGithub = async () => {
+  try {
+    const res = await signInWithPopup(auth, githubProvider);
+    const user = res.user;
+    console.log(user);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { app, auth, db, signInWithGoogle, signInWithGithub, logout };
