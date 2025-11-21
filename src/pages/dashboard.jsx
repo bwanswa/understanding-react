7a86322ebba12ec09bde4eb820a672de4301a788
bwanswa <deogratiousbwanswa@gmail.com>
2025-11-21 10:35:41 -0800

    Add files via upload

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,27 @@
+// src/pages/Dashboard.jsx
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+const Dashboard = () => {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login"); // redirect if not logged in
+    }
+  }, [user, loading, navigate]);
+
+  if (loading) return <h2>Loading...</h2>;
+
+  return (
+    <div>
+      <h1>Welcome to Dashboard</h1>
+      <p>User: {user?.email}</p>
+    </div>
+  );
+};
+
+export default Dashboard;
