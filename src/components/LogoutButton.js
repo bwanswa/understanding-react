6d378500a7b3ee3e80639f593f9c7dfb8e6248bf
bwanswa <deogratiousbwanswa@gmail.com>
2025-11-22 00:49:30 -0800

    Update LogoutButton.js

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,1 +1,9 @@
 
+import React from "react";
+import { logout } from "../firebase";
+
+function LogoutButton() {
+  return <button onClick={logout}>Logout</button>;
+}
+
+export default LogoutButton;
